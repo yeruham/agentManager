63870e6a9c4363669b692799bcc0034748175d66
YeruhamMendelson <y0583207549@gmail.com>
2025-06-08 12:34:09 +0300

    basic agent and basic DAL

---
+++ DAL/DALagents.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+
+public class DALAgents
+{
+    private string connectionStr = "server=localhost;user=root;password=;database=eagleeyedb";
+    private MySqlConnection connaction;
+
+    public DALAgents()
+    {
+        this.startConnection();
+    }
+    private void startConnection()
+    {
+        connaction = new MySqlConnection(connectionStr);
+    }
+
+    private void openConnection()
+    {
+        connaction.Open();
+    }
+
+    private void stopConnection()
+    {
+        connaction.Close();
+    }
+
+
+    public void addAgent(Agent agent)
+    {
+        int id = agent.id;
+        string codeName = agent.codeName;
+        string realName = agent.realName;
+        string location = agent.location;
+        string status = agent.status;
+        int missionsCompleted = agent.missionsCompleted;
+        
+        string sqlCommand = $"INSERT INTO agents(id, codeName, realName, location, status, missionsCompleted)" +
+            $" VALUSE({id},{codeName},{realName},{location},{status},{missionsCompleted})";
+
+
+    }
+
+}
