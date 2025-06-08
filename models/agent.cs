9c3365bac31183a0cca72022ebed4d57ac540b70
YeruhamMendelson <y0583207549@gmail.com>
2025-06-08 11:50:19 +0300

    start

---
+++ models/agent.cs
@@ -0,0 +1,14 @@
+﻿public class Agent
+{
+    public int id { get; set; }
+    
+    public string codeName { get; set; }
+    
+    public string realName { get; set; }
+    
+    public string location { get; set; }
+    
+    public string status { get; set; }
+    
+    public int missionsCompleted { get; set; }
+}
